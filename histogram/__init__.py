96e1957f9fe80707e2391493aee970c278e13437
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2015-06-02 22:45:21 +0200

    add stats to histogram

--- histogram/__init__.py
+++ histogram/__init__.py
@@ -1,12 +1,22 @@
+from __future__ import division, absolute_import
 import numpy as np
 import matplotlib.pyplot as plt
+from .runningstats import Statistics
 
 class Histogram():
-    def __init__(self, bins, limits=None):
+    """
+    A 1D Histogram that can be filled and stores some statistics
+    """
+    def __init__(self, bins, limits=None, include_all=False):
+        """
+        """
 
         if np.isscalar(bins):
             if limits is None:
-                raise ValueError('If you supply the number of bins you need to supply limits')
+                raise ValueError(
+                    'If you supply the number of bins'
+                    'you need to supply limits'
+                )
             self.n_bins = bins
             self.bin_edges = np.linspace(limits[0], limits[1], bins+1)
             self.limits = limits
@@ -27,8 +37,18 @@ class Histogram():
 
         self.bin_centers = self.bin_edges[:-1] + 0.5 * self.bin_width
 
+        self.stats = Statistics()
+        self.include_all = include_all
+
     def fill(self, data):
         data = np.array(data, ndmin=1, copy=False)
+
+        if self.include_all:
+            self.stats.fill(data)
+        else:
+            a, b = self.limits
+            in_limits = np.logical_and(data >= a, data <= b)
+            self.stats.fill(data[in_limits])
 
         entries, _ = np.histogram(data, self.bin_edges)
         self.entries += entries
@@ -58,3 +78,21 @@ class Histogram():
             )
 
         plt.draw_if_interactive()
+
+    def __len__(self):
+        return self.entries
+
+    def mean(self):
+        return self.stats.mean()
+
+    def std(self):
+        return self.stats.std()
+
+    def variance(self):
+        return self.stats.variance()
+
+    def kurtosis(self):
+        return self.stats.kurtosis()
+
+    def skewness(self):
+        return self.stats.skewness()
